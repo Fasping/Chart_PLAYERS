191c9dc1349d32eb3dafc7b6c04b4b0547116a51
Fernando Cases <nandocasesgarcia@gmail.com>
2022-09-13 15:40:09 +0200

    work in progress, start building mock api

---
+++ chart-react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <div>
+     <p>Hola</p>
+    </div>
+  )
+}
+
+export default App
---
+++ chart-react/src/components/api-player-mock.js
@@ -0,0 +1,28 @@
+const Player_Api = [
+  {
+    default: true,
+    duration: "7",
+    winloss: 33.35,
+    deposited: 5,
+  },
+  {
+    default: false,
+    duration: "30",
+    winloss: 33.35,
+    deposited: 55,
+  },
+  {
+    default: false,
+    duration: "90",
+    winloss: 33.35,
+    deposited: 55,
+  },
+  {
+    default: false,
+    duration: "180",
+    winloss: 33.35,
+    deposited: 55,
+  },
+];
+
+export default Player_Api
